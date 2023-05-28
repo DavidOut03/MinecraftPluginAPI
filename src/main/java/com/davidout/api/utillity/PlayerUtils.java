7b8c4cbbdbfc8209274b49f733f770ede3440435
David <davidoutdeveloper@gmail.com>
2023-05-28 14:51:45 +0200

    Added the abillity to changes players name tags.

---
+++ src/main/java/com/davidout/api/utillity/PlayerUtils.java
@@ -0,0 +1,38 @@
+package com.davidout.api.utillity;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.scoreboard.DisplaySlot;
+import org.bukkit.scoreboard.Objective;
+import org.bukkit.scoreboard.Scoreboard;
+import org.bukkit.scoreboard.Team;
+
+public class PlayerUtils {
+
+    public static void setNameTag(Player p, String prefix, String suffix) {
+        if(!ServerUtils.serverVersionIsAbove("1.7")) return;
+
+        Scoreboard scoreboard = Bukkit.getScoreboardManager().getMainScoreboard();
+        Team team = (scoreboard.getTeam(p.getName()) == null) ? scoreboard.registerNewTeam(p.getName()) : scoreboard.registerNewTeam(p.getName());
+        if(prefix != null && !prefix.isEmpty()) team.setPrefix(TextUtils.formatColorCodes(prefix));
+        if(suffix != null && !suffix.isEmpty()) team.setSuffix(TextUtils.formatColorCodes(suffix));
+        team.addEntry(p.getName());
+    }
+
+    public static void resetNameTag(Player p) {
+        Scoreboard scoreboard = Bukkit.getScoreboardManager().getMainScoreboard();
+        Team team = scoreboard.getTeam(p.getName());
+        if (team == null) return;
+            team.removeEntry(p.getName());
+            if (!team.getEntries().isEmpty()) return;
+                team.unregister();
+    }
+
+    public static void setTextBelowName(Player p, String text) {
+        Scoreboard scoreboard = p.getScoreboard();
+        Objective objective = scoreboard.registerNewObjective("belowName", "dummy", TextUtils.formatColorCodes(text));
+        objective.setDisplaySlot(DisplaySlot.BELOW_NAME);
+        objective.getScore(TextUtils.formatColorCodes(text)).setScore(0);
+
+    }
+}
--- src/main/java/com/davidout/api/utillity/ServerUtils.java
+++ src/main/java/com/davidout/api/utillity/ServerUtils.java
@@ -16,6 +16,21 @@ public class ServerUtils {
         return Double.parseDouble(versionParts[0] + "." + versionParts[1]);
     }
 
+    public static boolean serverVersionIsAbove(String version) {
+
+        String serverVersion = Bukkit.getBukkitVersion();
+        String[] serverParts = serverVersion.split("\\.");
+
+        String[] versionParts = version.split("\\.");
+
+        // Check if the first part is greater than 1 or if the second part is greater than or equal to 7
+        if (Integer.parseInt(serverParts[0]) > Integer.parseInt(versionParts[0]) || Integer.parseInt(serverParts[1]) >=  Integer.parseInt(versionParts[1])) {
+            return true;
+        }
+
+        return false;
+    }
+
 }
 
 
