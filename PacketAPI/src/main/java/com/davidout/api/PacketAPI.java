dfb9f49ee4ee83728f84d35c214b5101a94fd57f
David Out <david.out@migrato.nl>
2023-07-22 13:16:43 +0200

    Merged multiple packages

---
+++ PacketAPI/src/main/java/com/davidout/api/PacketAPI.java
@@ -0,0 +1,16 @@
+package com.davidout.api;
+
+import net.minecraft.network.protocol.game.PacketPlayOutChat;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Zombie;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.lang.reflect.InvocationTargetException;
+
+public final class PacketAPI extends JavaPlugin implements Listener {
+
+
+}
